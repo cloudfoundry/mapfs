d208e9411730fc204162665b5efb7c83009f66e1
Julian Hjortshoj <julian.hjortshoj@emc.com>
2017-12-04 17:10:17 -0800

    remove ineffective code that tries to set the euid back to the original value.  Remove excessive logging. [#153265024]

--- mapfs/mapfs.go
+++ mapfs/mapfs.go
@@ -47,13 +47,11 @@ func (fs *mapFileSystem) setEffectiveIDs(euid, egid int) (ouid, ogid int, err er
 		}
 	}
 
-	log.Printf("set-effective-ids old uid: %d, gid %d, desired uid %d, gid%d, actual uid %d gid %d\n", ouid, ogid, euid, egid, fs.syscall.Geteuid(), fs.syscall.Getegid())
 	return ouid, ogid, nil
 }
 
 func (fs *mapFileSystem) OnMount(nodeFs *pathfs.PathNodeFs) {
-	ouid, ogid, err := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	_, _, err := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,8 +60,7 @@ func (fs *mapFileSystem) OnMount(nodeFs *pathfs.PathNodeFs) {
 }
 
 func (fs *mapFileSystem) GetAttr(name string, context *fuse.Context) (a *fuse.Attr, code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 
 	a, code = fs.FileSystem.GetAttr(name, context)
 
@@ -80,127 +77,106 @@ func (fs *mapFileSystem) GetAttr(name string, context *fuse.Context) (a *fuse.At
 }
 
 func (fs *mapFileSystem) Chmod(name string, mode uint32, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Chmod(name, mode, context)
 }
 
 func (fs *mapFileSystem) Chown(name string, uid uint32, gid uint32, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Chown(name, uid, gid, context)
 }
 
 func (fs *mapFileSystem) Utimens(name string, Atime *time.Time, Mtime *time.Time, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Utimens(name, Atime, Mtime, context)
 }
 
 func (fs *mapFileSystem) Truncate(name string, size uint64, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Truncate(name, size, context)
 }
 
 func (fs *mapFileSystem) Access(name string, mode uint32, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Access(name, mode, context)
 }
 
 func (fs *mapFileSystem) Link(oldName string, newName string, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Link(oldName, newName, context)
 }
 
 func (fs *mapFileSystem) Mkdir(name string, mode uint32, context *fuse.Context) fuse.Status {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Mkdir(name, mode, context)
 }
 
 func (fs *mapFileSystem) Mknod(name string, mode uint32, dev uint32, context *fuse.Context) fuse.Status {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Mknod(name, mode, dev, context)
 }
 
 func (fs *mapFileSystem) Rename(oldName string, newName string, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Rename(oldName, newName, context)
 }
 
 func (fs *mapFileSystem) Rmdir(name string, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Rmdir(name, context)
 }
 
 func (fs *mapFileSystem) Unlink(name string, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Unlink(name, context)
 }
 
 func (fs *mapFileSystem) GetXAttr(name string, attribute string, context *fuse.Context) (data []byte, code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.GetXAttr(name, attribute, context)
 }
 
 func (fs *mapFileSystem) ListXAttr(name string, context *fuse.Context) (attributes []string, code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.ListXAttr(name, context)
 }
 
 func (fs *mapFileSystem) RemoveXAttr(name string, attr string, context *fuse.Context) fuse.Status {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.RemoveXAttr(name, attr, context)
 }
 
 func (fs *mapFileSystem) SetXAttr(name string, attr string, data []byte, flags int, context *fuse.Context) fuse.Status {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.SetXAttr(name, attr, data, flags, context)
 }
 
 func (fs *mapFileSystem) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Open(name, flags, context)
 }
 
 func (fs *mapFileSystem) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Create(name, flags, mode, context)
 }
 
 func (fs *mapFileSystem) OpenDir(name string, context *fuse.Context) (stream []fuse.DirEntry, code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.OpenDir(name, context)
 }
 
 func (fs *mapFileSystem) Symlink(value string, linkName string, context *fuse.Context) (code fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Symlink(value, linkName, context)
 }
 
 func (fs *mapFileSystem) Readlink(name string, context *fuse.Context) (string, fuse.Status) {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.Readlink(name, context)
 }
 
 func (fs *mapFileSystem) StatFs(name string) *fuse.StatfsOut {
-	ouid, ogid, _ := fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
-	defer fs.setEffectiveIDs(ouid, ogid)
+	fs.setEffectiveIDs(int(fs.uid), int(fs.gid))
 	return fs.FileSystem.StatFs(name)
 }
