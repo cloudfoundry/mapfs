b815b4acbf1ee4638bd5182cc9bd5f87a77f363c
Geoff Franks <geoff.franks@broadcom.com>
2024-06-10 16:33:54 +0000

    More ci updates - removing old yml + making run_fstest work

--- bin/test.bash
+++ bin/test.bash
@@ -3,7 +3,10 @@
 set -eu
 set -o pipefail
 
-./scripts/ci/run_fstest
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "${BUILD_ROOT_DIR}"
+"${THIS_FILE_DIR}/run_fstest"
+popd
 
 # shellcheck disable=SC2068
 # Double-quoting array expansion here causes ginkgo to fail
